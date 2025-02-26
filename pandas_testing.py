498c6756e3afb2f999214bdac961200e1963a4ce
Acieran <Rollersparks@yandex.ru>
2025-02-26 17:57:17 +0300

    More learning pandas

--- pandas_testing.py
+++ pandas_testing.py
@@ -2,6 +2,9 @@ import numpy as np
 
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import openpyxl
+
 s = pd.Series([1, 3, 5, np.nan, 6, 8])
 
 dates = pd.date_range("20130101", periods=6)
@@ -99,3 +102,133 @@ print('s.value_counts()', "\n",s.value_counts()) # Counts number of occurrences
 
 s = pd.Series(["A", "B", "C", "Aaba", "Baca", np.nan, "CABA", "dog", "cat"])
 print('s.str.lower()', "\n", s.str.lower()) # U can use string methods
+
+# Merge
+df = pd.DataFrame(np.random.randn(10, 4))
+print("Merge","\n",df)
+pieces = [df[:3], df[3:7], df[7:]]
+pd.concat(pieces)
+
+# Join
+left = pd.DataFrame({"key": ["foo", "foo"], "lval": [1, 2]})
+right = pd.DataFrame({"key": ["foo", "foo"], "rval": [4, 5]})
+print("left", "\n", left)
+print("right", "\n", right)
+print('pd.merge(left, right, on="key")', "\n", pd.merge(left, right, on="key"))
+
+# Merge on unique keys
+left = pd.DataFrame({"key": ["foo", "bar"], "lval": [1, 2]})
+right = pd.DataFrame({"key": ["foo", "bar"], "rval": [4, 5]})
+print("left", "\n", left)
+print("right", "\n", right)
+print('pd.merge(left, right, on="key")', "\n", pd.merge(left, right, on="key"))
+
+# Grouping
+df = pd.DataFrame(
+    {
+        "A": ["foo", "bar", "foo", "bar", "foo", "bar", "foo", "foo"],
+        "B": ["one", "one", "two", "three", "two", "two", "one", "three"],
+        "C": np.random.randn(8),
+        "D": np.random.randn(8),
+    }
+)
+print("Grouping", "\n", df)
+print('df.groupby("A")[["C", "D"]].sum()', "\n", df.groupby("A")[["C", "D"]].sum())
+print('df.groupby(["A", "B"]).sum()', "\n", df.groupby(["A", "B"]).sum())
+
+# Reshaping
+arrays = [
+   ["bar", "bar", "baz", "baz", "foo", "foo", "qux", "qux"],
+   ["one", "two", "one", "two", "one", "two", "one", "two"],
+]
+index = pd.MultiIndex.from_arrays(arrays, names=["first", "second"])
+df = pd.DataFrame(np.random.randn(8, 2), index=index, columns=["A", "B"])
+df2 = df[:4]
+print("Reshaping", "\n", df)
+print("Reshaping", "\n", df2)
+stacked = df2.stack(future_stack=True)
+print("The stack() method “compresses” a level in the DataFrame’s columns:","\n",stacked)
+print("""A 'stacked' DataFrame or Series (having a MultiIndex as the index),
+      the inverse operation of stack() is unstack(), 
+      which by default unstacks the last level:""")
+print("stacked.unstack()","\n",stacked.unstack())
+print("stacked.unstack(1)","\n",stacked.unstack(1))
+print("stacked.unstack(0)","\n",stacked.unstack(0))
+
+#Pivot tables
+df = pd.DataFrame(
+    {
+        "A": ["one", "one", "two", "three"] * 3,
+        "B": ["A", "B", "C"] * 4,
+        "C": ["foo", "foo", "foo", "bar", "bar", "bar"] * 2,
+        "D": np.random.randn(12),
+        "E": np.random.randn(12),
+    }
+)
+print("Pivot tables")
+print("df", "\n", df)
+print('pd.pivot_table(df, values="D", index=["A", "B"], columns=["C"])', "\n", pd.pivot_table(df, values="D", index=["A", "B"], columns=["C"]))
+
+#Time series
+print("Time series")
+rng = pd.date_range("1/1/2012", periods=100, freq="s")
+print("rng", rng)
+ts = pd.Series(np.random.randint(0, 500, len(rng)), index=rng)
+print("ts", ts)
+print('ts.resample("1Min").sum()',"\n",ts.resample("1Min").sum())
+
+rng = pd.date_range("3/6/2012 00:00", periods=5, freq="D")
+ts = pd.Series(np.random.randn(len(rng)), rng)
+print("ts", "\n", ts)
+ts_utc = ts.tz_localize("Europe/Moscow")
+print("Series.tz_localize() localizes a time series to a time zone:", "\n",ts_utc)
+print('Series.tz_convert() converts a timezones aware time series to another time zone:',"\n", ts_utc.tz_convert("US/Eastern"))
+print('Adding a non-fixed duration (BusinessDay) to a time series:',"\n", "rng", "\n", rng,
+      "\n", 'rng + pd.offsets.BusinessDay(5)', "\n",rng + pd.offsets.BusinessDay(5))
+
+
+print("""Categoricals
+pandas can include categorical data in a DataFrame. For full docs, see the categorical introduction and the API documentation.""")
+df = pd.DataFrame(
+    {"id": [1, 2, 3, 4, 5, 6], "raw_grade": ["a", "b", "b", "a", "a", "e"]}
+)
+df["grade"] = df["raw_grade"].astype("category")
+print(df)
+print('df["grade"]',"\n",df["grade"])
+
+new_categories = ["very good", "good", "very bad"]
+df["grade"] = df["grade"].cat.rename_categories(new_categories)
+print("Renaming the categorical columns:", "\n", df)
+print("Reorder the categories and simultaneously add the missing categories (methods under Series.cat() return a new Series by default):")
+df["grade"] = df["grade"].cat.set_categories(
+    ["very bad", "bad", "medium", "good", "very good"]
+)
+print(df["grade"])
+print('df.sort_values(by="grade")',"\n",df.sort_values(by="grade", ascending=False))
+print('df.groupby("grade", observed=False).size()',"\n",df.groupby("grade", observed=False).size())
+
+
+print("Plotting")
+plt.close("all")
+ts = pd.Series(np.random.randn(1000), index=pd.date_range("1/1/2000", periods=1000))
+ts = ts.cumsum()
+ts.plot()
+df = pd.DataFrame(
+    np.random.randn(1000, 4), index=ts.index, columns=["A", "B", "C", "D"]
+)
+df = df.cumsum()
+plt.figure()
+df.plot()
+plt.legend(loc='best')
+# plt.show()
+
+
+print("\n\nImporting and exporting data")
+print("CSV")
+df = pd.DataFrame(np.random.randint(0, 5, (10, 5)))
+df.to_csv("foo.csv")
+print('pd.read_csv("foo.csv")', "\n", pd.read_csv("foo.csv"))
+print("Exel")
+df.to_excel("foo.xlsx", sheet_name="Sheet1")
+print('pd.read_excel("foo.xlsx", "Sheet1", index_col=None, na_values=["NA"])\n', pd.read_excel("foo.xlsx", "Sheet1", index_col=None, na_values=["NA"]))
+
